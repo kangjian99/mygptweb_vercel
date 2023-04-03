c72a1bac5c867b2a9c2a7bf3d22af42e2205da9e
kangjian99 <radiokj@163.com>
2023-04-03 19:43:29 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -4,12 +4,8 @@ import openai
 # openai.api_key = os.environ.get('OPENAI_API_KEY')
 model = 'gpt-3.5-turbo' # or text-davinci-003
 SESSION_SECRET_KEY = os.environ.get('SESSION_SECRET_KEY')
+
 MONGODB_URI = os.environ.get('MONGODB_URI')
-server = 'tcp:kj99.database.windows.net,1433'
-database = 'database'
-db_username = os.environ.get('DB_USERNAME')
-db_password = os.environ.get('DB_PASSWORD')
-driver = '{ODBC Driver 18 for SQL Server}'
 
 openai_api_key = os.environ.get('OPENAI_API_KEY')
 API_URL = "https://api.openai.com/v1/chat/completions"
